7ae6bbb389cd12d2679612e5696c326260f4f2d0
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2021-05-28 08:28:42 +0900

    init

---
+++ lib/tesla_aws_sigv4.ex
@@ -0,0 +1,18 @@
+defmodule TeslaAwsSigv4 do
+  @moduledoc """
+  Documentation for `TeslaAwsSigv4`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeslaAwsSigv4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesla_aws_sigv4_test.exs
@@ -0,0 +1,8 @@
+defmodule TeslaAwsSigv4Test do
+  use ExUnit.Case
+  doctest TeslaAwsSigv4
+
+  test "greets the world" do
+    assert TeslaAwsSigv4.hello() == :world
+  end
+end
